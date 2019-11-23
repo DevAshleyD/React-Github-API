e17559be9ba0f351b1878fec435f269f639364ab
Ashley D <webdevmvp@gmail.com>
2019-11-23 03:27:04 +0000

    Updates

---
+++ precache-manifest.b4d870d294e00fc8ac6d81170387657d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e073e0f6d9507c3f724",
+    "url": "/React-Github-API/static/js/runtime~main.6e073e0f.js"
+  },
+  {
+    "revision": "c5260b7befc6b0d15813",
+    "url": "/React-Github-API/static/js/main.c5260b7b.chunk.js"
+  },
+  {
+    "revision": "b740b73c85a42ea9ccf5",
+    "url": "/React-Github-API/static/js/1.b740b73c.chunk.js"
+  },
+  {
+    "revision": "c5260b7befc6b0d15813",
+    "url": "/React-Github-API/static/css/main.6e1ef5d9.chunk.css"
+  },
+  {
+    "revision": "63ac33adee51c8bac91599769e4a2e0c",
+    "url": "/React-Github-API/index.html"
+  }
+];
